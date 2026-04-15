60d087a81145431527906ed10df0c66c5bbe4549
aldairZP <jonathan.aldair.personal@gmail.com>
2026-04-14 21:02:29 -0600

    feat: Implement CreateAsync method in UserService for user creation

--- EnglishAcademy.Application/Services/UserService.cs
+++ EnglishAcademy.Application/Services/UserService.cs
@@ -1,5 +1,8 @@
-using System;
+using System.Runtime.InteropServices;
 using EnglishAcademy.Application.Abstractions.Persistence;
+using EnglishAcademy.Application.Contracts;
+using EnglishAcademy.Application.Contracts.Users;
+using EnglishAcademy.Domain.Entities;
 
 namespace EnglishAcademy.Application.Services;
 
@@ -9,6 +12,14 @@ public class UserService
     public UserService(IUserRepository userRepository)
     {
         _userRepository = userRepository;
+    }
+
+    public async Task<UserDto> CreateAsync(CreateUserRequest createUserRequest)
+    {
+        var newUser = new User(createUserRequest.UserName, createUserRequest.Password);
+        await _userRepository.AddAsync(newUser);
+
+        return new UserDto(newUser.Id, newUser.UserName);
     }
 
 
